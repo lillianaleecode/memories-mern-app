8c2abe7993dae6cbc3fcf4a251d7b49b8f02228f
lillianaleecode <lilliana.lee@code.berlin>
2022-05-18 16:21:36 +0200

    created folders organizely (for scability), and created basic front end template for Posts

---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Form = () =>{
+    return (
+        <h1>Form</h1>
+    )
+}
+
+export default Form;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import Post from './Post/Post';
+
+const Posts = () =>{
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+            <Post />
+
+            
+        </>
+        
+    )
+}
+
+export default Posts;
