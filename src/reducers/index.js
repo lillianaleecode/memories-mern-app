ed289422c827ade8ab08be1f7a84a5287a1750ef
lillianaleecode <lilliana.lee@code.berlin>
2022-05-20 17:44:05 +0200

    setting redux

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import posts from './posts';
+
+export default combineReducers({
+     // posts:posts,
+     posts //since key and value is the same, we just use posts. 
+})
---
+++ src/reducers/posts.js
@@ -0,0 +1,22 @@
+export default (posts = [], action) => { //since this reducer is not being used, for now just function export default. 
+    switch(action.type) {
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;
+    }
+}
+
+// //since this reducer is not being used, for now just function export default. 
+// const reducer = (posts, action) => { 
+//     switch(action.type) {
+//         case 'FETCH_ALL':
+//             return posts;
+//         case 'CREATE':
+//             return posts;
+//         default:
+//             return posts;
+//     }
+// }
